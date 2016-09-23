0d8cefde579099bc908fe145407eb2c8cf6a7d89
Gerson Cardoso Filho <gersoncfilho@mac.com>
2016-09-22 22:40:03 -0300

    Fragment de teste

---
+++ app/src/main/java/br/com/gersoncardoso/reddittest4/Adapter/PostsAdapter.java
@@ -0,0 +1,63 @@
+package br.com.gersoncardoso.reddittest4.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.gersoncardoso.reddittest4.Model.Post;
+import br.com.gersoncardoso.reddittest4.R;
+
+/**
+ * Created by gerso on 22/09/2016.
+ */
+
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder> {
+
+    private String[] mDataSet;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public CardView mCardView;
+        public TextView post_score, post_details, post_title;
+        public MyViewHolder(View view)
+        {
+            super(view);
+            mCardView = (CardView) view.findViewById(R.id.card_view);
+            post_details = (TextView) view.findViewById(R.id.post_details);
+            post_score = (TextView) view.findViewById(R.id.post_score);
+            post_title = (TextView) view.findViewById(R.id.post_title);
+        }
+    }
+
+    public PostsAdapter(String[] myDataSet)
+    {
+        mDataSet = myDataSet;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.new_fragment, parent, false);
+        //set views size, margins, paddings and layout params
+
+        MyViewHolder vh = new MyViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.post_title.setText("title");
+        holder.post_details.setText("details");
+        holder.post_score.setText("score");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+}
--- app/src/main/java/br/com/gersoncardoso/reddittest4/Fragment/NewFragment.java
+++ app/src/main/java/br/com/gersoncardoso/reddittest4/Fragment/NewFragment.java
@@ -3,10 +3,13 @@ package br.com.gersoncardoso.reddittest4.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import br.com.gersoncardoso.reddittest4.Adapter.PostsAdapter;
 import br.com.gersoncardoso.reddittest4.R;
 
 /**
@@ -15,10 +18,30 @@ import br.com.gersoncardoso.reddittest4.R;
 
 public class NewFragment extends Fragment {
 
+    public NewFragment()
+    {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.new_fragment, container, false);
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.rv_recycler_view);
+        rv.setHasFixedSize(true);
+
+        PostsAdapter postsAdapter = new PostsAdapter(new String[] {"teste um", "teste dois", "teste tres"});
+        rv.setAdapter(postsAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(linearLayoutManager);
+
         return view;
     }
 }
